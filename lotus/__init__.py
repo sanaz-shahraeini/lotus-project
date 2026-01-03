0589b99c16c27c5a93dd92d28ad3f7576e5b9ec4
sanaz <s.shahraeini@gmail.com>
2026-01-03 19:42:36 +0330

    refactor stylings3

--- lotus/__init__.py
+++ lotus/__init__.py
@@ -1,8 +1,4 @@
 from __future__ import absolute_import, unicode_literals
-import os
+from .celery import app as celery_app
 
-if not (os.getenv('VERCEL') == '1' or os.getenv('VERCEL')):
-    from .celery import app as celery_app
-    __all__ = ('celery_app',)
-else:
-    __all__ = tuple()
+__all__ = ('celery_app',)
--- lotus/wsgi.py
+++ lotus/wsgi.py
@@ -9,12 +9,8 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 
 import os
 
-import sys
-
 from django.core.wsgi import get_wsgi_application
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'lotus.settings')
 
 application = get_wsgi_application()
-app = application
